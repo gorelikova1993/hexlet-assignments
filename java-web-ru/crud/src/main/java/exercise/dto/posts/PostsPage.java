7152636043714aa1a9f6e47a029f478f6d5d67bd
@hexlet/cli <support@hexlet.io>
2024-10-31 19:48:35 +0300

    submit java-web-ru/crud

---
+++ java-web-ru/crud/src/main/java/exercise/dto/posts/PostsPage.java
@@ -0,0 +1,19 @@
+package exercise.dto.posts;
+
+import java.util.List;
+import exercise.model.Post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+// BEGIN
+@AllArgsConstructor
+@Getter
+public class PostsPage {
+    public List<Post> posts;
+    public int pageNumber;
+}
+// END
+
+
