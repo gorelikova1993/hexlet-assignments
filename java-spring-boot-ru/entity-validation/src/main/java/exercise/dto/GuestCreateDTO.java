ff4c220388c2553fd20762437202eed3d5389f74
@hexlet/cli <support@hexlet.io>
2025-04-10 16:50:19 +0300

    submit java-spring-boot-ru/entity-validation

--- java-spring-boot-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
+++ java-spring-boot-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -1,10 +1,13 @@
 package exercise.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 // BEGIN
 @Setter
@@ -18,6 +21,12 @@ public class GuestCreateDTO {
 
     @Pattern(regexp = "^\\+[0-9]{11,13}$", message = "Номер телефона должен начинаться с '+' и содержать от 11 до 13 цифр")
     private String phoneNumber;
+
+    @Pattern(regexp = "^[0-9]{4}$")
+    private String clubCard;
+
+    @Future(message = "Дата события должна быть в будущем")
+    private LocalDate cardValidUntil;
 
 }
 // END
