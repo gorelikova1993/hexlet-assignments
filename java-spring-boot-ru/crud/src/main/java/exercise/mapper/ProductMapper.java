94e20ecd749d6ad18134fd13f9d30437885cd4db
@hexlet/cli <support@hexlet.io>
2025-05-19 15:35:27 +0300

    submit java-spring-boot-ru/crud

--- java-spring-boot-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package exercise.mapper;
 import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductUpdateDTO;
+import exercise.model.Category;
 import exercise.model.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -19,10 +20,29 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class ProductMapper {
+
+    @Mapping(source = "categoryId", target = "category")
     public abstract Product map(ProductCreateDTO dto);
 
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "category.name", target = "categoryName")
     public abstract ProductDTO map(Product model);
 
-    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
+    public  void update(ProductUpdateDTO dto, @MappingTarget Product product) {
+        if (dto.getCategoryId() != null && dto.getCategoryId().isPresent()) {
+            Long categoryId = dto.getCategoryId().get();
+            Category category = new Category();
+            category.setId(categoryId);
+            product.setCategory(category);
+        }
+
+        if (dto.getTitle() != null && dto.getTitle().isPresent()) {
+            product.setTitle(dto.getTitle().get());
+        }
+
+        if (dto.getPrice() != null && dto.getPrice().isPresent()) {
+            product.setPrice(dto.getPrice().get());
+        }
+    }
 }
 // END
